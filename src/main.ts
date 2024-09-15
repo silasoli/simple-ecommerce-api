4204bdbdaf61d4d1e40a3fa12a9aa6d010d50280
silasoli <silasoliv39@gmail.com>
2024-09-15 14:51:45 -0300

    feat: adding cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ import { TypeORMExceptionFilter } from './common/exception-filters/typeorm-excep
 import { EntityNotFoundExceptionFilter } from './common/exception-filters/entity-not-found-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
 
   const configService = app.get(ConfigService);
 
