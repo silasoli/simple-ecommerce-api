5e6c3384fe2414ab9a0a18392e025652cc699dea
silasoli <silasoliv39@gmail.com>
2024-09-15 14:57:29 -0300

    feat: adding cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000', // Permitir o frontend acessar a API
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    // allowedHeaders: 'Content-Type, Authorization',
+    // credentials: true, // Se precisar enviar cookies
   });
 
   const configService = app.get(ConfigService);
