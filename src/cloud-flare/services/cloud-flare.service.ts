aa787660e7bec3e5f94dba4b9c7412c4f89024d2
silasoli <silasoliv39@gmail.com>
2024-09-17 19:45:13 -0300

    feat: adding form data

--- src/cloud-flare/services/cloud-flare.service.ts
+++ src/cloud-flare/services/cloud-flare.service.ts
@@ -1,8 +1,12 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import * as FormData from 'form-data';
+// import * as FormData from 'form-data';
 import { lastValueFrom } from 'rxjs';
 import { CLOUD_FLARE_ERRORS } from '../constants/cloud-flare.errors';
+// import FormData from 'form-data';
+import * as FormData from 'form-data';
+
+
 
 @Injectable()
 export class CloudFlareService {
@@ -16,13 +20,46 @@ export class CloudFlareService {
   private validUploadImage(file: Express.Multer.File): void {
     const allowedMimeTypes = ['image/jpeg', 'image/png'];
 
-    if (!allowedMimeTypes.includes(file.mimetype)) 
+    if (!allowedMimeTypes.includes(file.mimetype))
       throw CLOUD_FLARE_ERRORS.INVALID_IMAGE;
 
     const maxSize = 10 * 1024 * 1024;
 
     if (file.size > maxSize) throw CLOUD_FLARE_ERRORS.INVALID_SIZE;
   }
+
+  // async uploadImage(file: Express.Multer.File): Promise<string> {
+  //   this.validUploadImage(file);
+
+  //   const filename = file.originalname;
+  //   const image = file.buffer;
+
+  //   try {
+  //     const formData = new FormData();
+  //     formData.append('file', image, {
+  //       filename: filename,
+  //       contentType: 'image/jpeg',
+  //     });
+
+  //     const response = await lastValueFrom(
+  //       this.httpService.post(
+  //         `https://api.cloudflare.com/client/v4/accounts/${this.accountId}/images/v1`,
+  //         formData,
+  //         {
+  //           headers: {
+  //             ...formData.getHeaders(),
+  //             Authorization: this.apiKey,
+  //           },
+  //         },
+  //       ),
+  //     );
+
+  //     return response.data.result.variants[0];
+  //   } catch (error) {
+  //     this.logger.error('Erro ao fazer upload da imagem:', error.message);
+  //     throw CLOUD_FLARE_ERRORS.UPLOAD_IMAGE;
+  //   }
+  // }
 
   async uploadImage(file: Express.Multer.File): Promise<string> {
     this.validUploadImage(file);
@@ -34,7 +71,7 @@ export class CloudFlareService {
       const formData = new FormData();
       formData.append('file', image, {
         filename: filename,
-        contentType: 'image/jpeg',
+        contentType: file.mimetype, // Você pode usar o mimetype do arquivo aqui
       });
 
       const response = await lastValueFrom(
